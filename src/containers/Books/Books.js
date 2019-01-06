de4436437198eab1a9802246f2ff2028041d7186
Ashton Bailey <ashtonkbailey@gmail.com>
2019-01-06 15:57:09 -0700

    Update Books to reflect correct route

--- src/containers/Books/Books.js
+++ src/containers/Books/Books.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 import '../../index.scss';
 
-const Books = () => {
+const Books = (props) => {
+  const { type } = this.props;
+  let text;
+
+  if (type === 'confirm') {
+    text = "Confirm your loved book:"
+  } else {
+    // need to bring in book title from lovedBook in store
+    text = "Here are some recommendations based on :"
+  }
   return (
     <div>
+      <h2>{text}</h2>
+      <hr />
+      <div className="books-container">
+
+      </div>
     </div>
   )
 }
